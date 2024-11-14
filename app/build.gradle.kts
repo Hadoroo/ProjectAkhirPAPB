3231cc9a98380790523c455e9e7a2f1e01a9cff9
I Wayan Ivan Zenatmaja <iwayanivanz@student.ub.ac.id>
2024-11-14 12:55:00 +0700

    aaaa
    aaaaaaa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.crashlytics.buildtools)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -68,4 +69,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation (libs.androidx.navigation.compose)
+    implementation (libs.androidx.room.runtime)
+    implementation (libs.androidx.room.ktx)
+
 }
